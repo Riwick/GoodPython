d6d7d06b901e8891df7d5c21ec07ba11d7075ccf
Rodion <rodbel229@gmail.com>
2024-05-29 18:45:26 +0500

    maths

---
+++ variable.py
@@ -0,0 +1,23 @@
+a = 6.8
+print(type(a), id(a))
+
+b = a
+print(type(b), id(b))
+
+a, b = b, a
+print(b is a)
+
+x = 5.6
+s = "5.6"
+print(type(x), type(s))
+
+msg = "Сообщение"
+count = 0
+
+arg = 0
+Arg = 0
+
+# True = 5
+
+print_ = 5
+# print(5)
